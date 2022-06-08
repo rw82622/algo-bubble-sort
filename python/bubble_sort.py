db316b4122826c4e32d1527de2b4a0d61d8887e6
Rexford Wiafe <w.rexford@yahoo.com>
2022-06-08 18:29:04 -0500

    solve bubble_sort algo in python

--- python/bubble_sort.py
+++ python/bubble_sort.py
@@ -1,7 +1,23 @@
+
+def bubble_sort(arr):
+    swaps = 0
+    ops = 0
+    for j in range(len(arr)):
+        swapped = False
+        for i in range (1, len(arr)-j):
+            ops += 1
+            if arr[i] < arr[i-1]:
+                arr[i-1], arr[i] = arr[i], arr[i-1]
+                swaps += 1
+                swapped = True
+        if not swapped:
+            break
+    print(f"Final result: {arr}")
+    print(f"Swaps: {swaps} Operations: {ops}")
+    
+    
 sequence = [4, 3, 5, 0, 1]
-swaps = 0
+bubble_sort(sequence)
+            
 
-# Your Code Here
 
-print(f"Final result: {result}")
-print(f"Swaps: {swaps}")
