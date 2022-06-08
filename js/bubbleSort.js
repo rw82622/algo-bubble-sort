b5f0a47c966d64348fdccf9f030e15afd5c6e80a
Rexford Wiafe <w.rexford@yahoo.com>
2022-06-08 18:47:01 -0500

    solve bubbleSort challenge in js

--- js/bubbleSort.js
+++ js/bubbleSort.js
@@ -1,7 +1,25 @@
-var sequence = [4, 3, 5, 0, 1]
-var swaps = 0
+var swaps = 0;
 
-// Your Code Here
+function bubbleSort(arr) {
+  let swaps = 0;
+  let ops = 0;
+  for (let i = 0; i < arr.length; i++) {
+    let swapped = false;
+    for (let j = 1; j < arr.length - i; j++) {
+      ops += 1;
+      if (arr[j] < arr[j - 1]) {
+        [arr[j - 1], arr[j]] = [arr[j], arr[j - 1]];
+        swaps += 1;
+        swapped = true;
+      }
+    }
+    if (!swapped) {
+      break;
+    }
+  }
+  console.log(`Final result: ${arr}`);
+  console.log(`Swaps: ${swaps} Operations: ${ops}`);
+}
 
-console.log("Final result: " + result)
-console.log("Swaps: " + swaps)
+var sequence = [4, 3, 5, 0, 1];
+bubbleSort(sequence);
